38d70453d77cf632efaa76851284ffdca112ccc6
Nabin <sharmanabin.0910@gmail.com>
2025-11-13 23:46:06 +0530

    Beginning strings'

---
+++ Strings/handson.py
@@ -0,0 +1,17 @@
+chai = 'masala chai'
+first_char = chai[0]
+print(first_char) # m
+
+slice_chai = chai[0 : 6]
+print(slice_chai) #masala
+
+num_list = "0123456789"
+
+
+print(num_list[3:])
+print(num_list[:7])
+# 3456789
+# 0123456
+
+print(num_list[0:7:2]) #third parameter stands for hopping
+
